5be801b74e72224685697b1bc0e25b85d0e8d75f
nikatopu <nikatopu@gmail.com>
2024-08-22 13:14:55 +0400

    Added logging in and registering
    - Added NodeJS code for logging in and registering functionality, while also creating usernames
    
    Note: God damn this was hard

--- index.js
+++ index.js
@@ -63,8 +63,134 @@ app.get("/register", (req, res) => {
     res.render("register.ejs");
 })
 
-app.get("/user", (req, res) => {
-    res.render("user.ejs");
+app.get("/user", async (req, res) => {
+    if (req.isAuthenticated()) {
+        const result = await db.query("SELECT * FROM person_data WHERE id=$1", [req.user.id])
+        console.log(result);
+
+        res.render("user.ejs", {data: result.rows[0]});
+    } else {
+        res.redirect("/login")
+    }
+})
+
+// Logging out
+app.get("/logout", (req, res) => {
+    req.logOut(function (err) {
+        if (err) {return next(err);}
+        res.redirect("/");
+    });
+});
+
+// Registering
+app.post("/register", async (req, res) => {
+    // Get the email and the password
+    const email = req.body.username;
+    const password = req.body.password;
+    const repeatpassword = req.body.repeatpassword;
+
+
+    try {
+        // Check if the passwords match
+        if (password === repeatpassword) {
+
+            // Check if user is already registered
+            const checkResult = await db.query(
+                "SELECT * FROM person WHERE email=$1",
+                [email]
+            );
+            
+            if (checkResult.rows.length > 0) {
+                // If already registered, redirect to login
+                req.redirect("/login");
+            } else {
+                // If not, hash the password and store the data
+                bcrypt.hash(password, saltRounds, async (err, hash) =>{
+                    if (err) {console.error("error hashing: ", err)}
+                    else {
+                        const result = await db.query(
+                            "INSERT INTO person (email, password) VALUES ($1, $2) RETURNING *",
+                            [email, hash]
+                        );
+                        const user = result.rows[0];
+
+                        // Also create a display name and a pfp link for the user and store as data
+                        const displayName = "user" + user.id;
+                        const data = await db.query(
+                            "INSERT INTO person_data (id, pfplink, displayname) VALUES ($1, $2, $3) RETURNING *",
+                            [user.id, "/icons/user-holder.png", displayName]
+                        )
+
+                        // Login the user
+                        req.login(user, (err) => {
+                            console.log("user has been registered successfully");
+                            res.redirect("/user");
+                        })
+                    }
+                })
+            }
+        } else {
+            // If password and repeat password don't match, throw an error
+            res.redirect("/register");
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+    
+})
+
+// Logging in
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/user",
+    failureRedirect: "/login",
+}));
+
+// Passport Strategy (Local)
+passport.use("local", new Strategy(
+    // Make sure that the names are username and password in login and register pages
+    async function verify(username, password, cb) {
+        try {
+            // Get user data
+            const result = await db.query(
+                "SELECT * FROM person WHERE email=$1",
+                [username]
+            );
+
+            if (result.rows.length > 0) {
+                // If such user exists, compare the bcrypt hashes
+                const user = result.rows[0];
+                const storedHash = user.password;
+                
+                bcrypt.compare(password, storedHash, (err, valid) => {
+                    if (err) {
+                        return cb(err);
+                    } else {
+                        if (valid) {
+                            // Return the user if everything goes well
+                            return cb(null, user);
+                        } else {
+                            return cb(null, false);
+                        }
+                    }
+                });
+            } else {
+                return cb("User not found");
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+))
+
+// Passport Serialization
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 })
 
 // Start the server
